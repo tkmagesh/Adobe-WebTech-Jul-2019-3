8a90a7c5f4cff2a7f9a950f837b357756553ce7b
Magesh Kuppan <tkmagesh77@gmail.com>
2019-07-29 15:20:33 +0530

    fp assignment

---
+++ 01-Intro/products.js
@@ -0,0 +1,27 @@
+var products = [
+	{id : 6, name : 'Pen', cost : 50, units : 20, category : 'stationary'},
+	{id : 9, name : 'Ten', cost : 70, units : 70, category : 'stationary'},
+	{id : 3, name : 'Len', cost : 60, units : 60, category : 'grocery'},
+	{id : 5, name : 'Zen', cost : 30, units : 30, category : 'grocery'},
+	{id : 1, name : 'Ken', cost : 20, units : 80, category : 'utencil'},
+];
+
+Array methods:
+	filter
+	forEach
+	map
+	reduce
+
+1. find out all the stationary products
+
+2. find out the maximum cost from the product list
+
+3. create a new array of products where the cost is discounted by 10%
+
+4. find the sum of units 
+
+5. print the following for each product
+		We have 20 units of pen at Rs.50 each
+
+
+Important Note :  DO NOT USE the FOR loop
